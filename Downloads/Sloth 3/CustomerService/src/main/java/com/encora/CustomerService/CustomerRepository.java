631439b6babdc9ea2b809467e3a0657708a04a17
rajathmg18 <rajath.mg@encora.com>
2025-10-06 12:38:22 +0530

    Delete Downloads/Sloth 3 directory

--- Downloads/Sloth 3/CustomerService/src/main/java/com/encora/CustomerService/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.encora.CustomerService;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository  extends JpaRepository<Customer, Long>{
-
-}
--- Downloads/Sloth 3/OrderService/src/main/java/com/encora/OrderService/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.encora.OrderService.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.encora.OrderService.Model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
